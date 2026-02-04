08e186de1e69c8a1e40c8573072ee1ff48a8a50d
abdul rasheed <abdul.rasheed@simpleenergy.in>
2026-02-04 07:41:49 +0530

    spring initialed skeleton

---
+++ src/main/java/com/fs_oms_spring_boot/order_service/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fs_oms_spring_boot.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fs_oms_spring_boot/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs_oms_spring_boot.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
